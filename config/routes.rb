7cfaea5570ed7dabacffe1fbb3f329127f344380
Yasir Malik <yasirrafiq786@gmail.com>
2020-09-02 17:30:49 +1000

    Added namespaced routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :items
-  resources :lists
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :v1 do
+    namespace :api do
+      resources :items
+      resources :lists
+    end
+  end
 end
